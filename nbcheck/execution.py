93f3dd9debb86930d394940580c128b6c35c5c93
Ludovico Bianchi <lbianchi@lbl.gov>
2023-03-29 00:38:34 -0500

    Refactor how settings are set and stored within execution plugin

--- nbcheck/execution.py
+++ nbcheck/execution.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from dataclasses import dataclass
 import os
 from pathlib import Path
 import shutil
@@ -11,9 +12,15 @@ from nbval import plugin as nbval_plugin
 from nbcheck.api import Notebook
 
 
-kernel_name = pytest.StashKey[str]()
-kernelspec_path = pytest.StashKey[Path]()
-current_pid = pytest.StashKey[int]()
+@dataclass
+class Settings:
+    kernel_name: str = ""
+    kernelspec_path: Path = None
+    current_pid: int = os.getpid()
+    cell_timeout_s: int = -1
+
+
+settings = pytest.StashKey[Settings]()
 
 
 @contextmanager
@@ -32,25 +39,26 @@ def temp_kernelspec(name: str, **kwargs) -> Path:
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config: pytest.Config):
-    pid = os.getpid()
-    kname = f"nbcheck-kernel-{pid}"
-    config.pluginmanager.unregister(nbval_plugin)
 
+    s = config.stash[settings] = Settings()
     opt = config.option
 
     opt.nbval_lax = True
     opt.nbval_current_env = False
-    opt.nbval_kernel_name = kname
+    opt.nbval_kernel_name = s.kernel_name = f"nbcheck-kernel-{s.current_pid}"
+    opt.nbval_cell_timeout = s.cell_timeout_s = int(opt.nbcheck_cell_timeout_s)
 
-    config.stash[current_pid] = pid
-    config.stash[kernel_name] = kname
+    config.pluginmanager.unregister(nbval_plugin)
 
 
 def pytest_nbcheck_info(config: pytest.Config) -> List[str]:
-    kname = config.stash[kernel_name]
+    s = config.stash[settings]
     lines = [
-        f"Notebooks will be executed using kernel: {kname!r}",
+        f"Notebooks will be executed using kernel: {s.kernel_name!r}",
         "It will be removed at the end of the run.",
+    ]
+    lines += [
+        f"Cell timeout set to {s.cell_timeout_s} s."
     ]
     return lines
 
@@ -65,9 +73,8 @@ def pytest_nbcollect_makeitem(collector: Notebook):
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtestloop(session: pytest.Session):
-    stash = session.config.stash
-    kname = stash[kernel_name]
+    s = session.config.stash[settings]
 
-    with temp_kernelspec(kname) as kpath:
-        stash[kernelspec_path] = kpath
+    with temp_kernelspec(s.kernel_name) as kpath:
+        s.kernelspec_path = kpath
         yield
